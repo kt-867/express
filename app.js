35a7ba8834c1d306e5f6fa3d18c2eddf3362a676
kt-867 <no.867jmbox0@gmail.com>
2025-11-26 15:44:14 +0900

    add cat api

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var helloRouter = require('./routes/hello');
-var notesRouter = require('./routes/notes'); 
+var notesRouter = require('./routes/notes');
+var catRouter = require('./routes/cat'); 
 
 var app = express();
 
@@ -16,6 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter); 
+app.use('/cat', catRouter);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
